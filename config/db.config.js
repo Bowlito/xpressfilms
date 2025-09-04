487207a1e4dd424a8aa1b9bd63ba50f662869b2b
Bowlito <julien.catoio@gmail.com>
2025-09-04 16:10:32 +0200

    install ejs

---
+++ config/db.config.js
@@ -0,0 +1,25 @@
+import * as mysql from 'mysql2/promise';
+
+const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+// connection.connect((err)=> {
+//     if (err) {
+//         console.log(err);
+        
+//     } else {
+//         console.log(`Connexion établie avec MySql`);
+        
+//     }
+// })
+connection
+    .connect()
+    .then(() => console.log(`Connexion établie avec MySql`))
+    .catch(err => console.log(err));
+
+export default connection;
