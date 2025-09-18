28bc54d3d8fd7f03716bf95742124cb76e3b127c
Mr_Boule <julien.catoio@gmail.com>
2025-09-18 16:15:11 +0200

    C'est bon !

--- controllers/favoris.controller.js
+++ controllers/favoris.controller.js
@@ -3,7 +3,12 @@ import favorisRepository from "../repositories/favoris.repository.js";
 const show = async (req, res, next) => {
     try {
         const user = req.session.user
-        const userFav = await favorisRepository.findFavs(user.id)
+        if (!user) {
+            console.log("Pas d'utilisateur");
+            
+            return res.redirect("/connexion")
+        }
+        const userFav = await favorisRepository.findFavs(user)
         res.render("favoris", {
             favoris: userFav,
             erreurs: null
--- repositories/favoris.repository.js
+++ repositories/favoris.repository.js
@@ -1,17 +1,15 @@
 import connection from "../config/db.config.js";
 
 const findFavs = async (user) => {
-    const SELECT = "SELECT * FROM favoris WHERE id_utilisateur=?"
-    try {
-        const resultat = await connection.query(SELECT, [user.id])
-        console.log(resultat);
-        return resultat[0]
-        
-    } catch (error) {
-        console.log(error);
-        return null
-        
-    }
-}
+  const SELECT = "SELECT f.* FROM favoris AS fav JOIN films AS f ON fav.id_film = f.id_film WHERE fav.id_utilisateur=?";
+  try {
+    const resultat = await connection.query(SELECT, [user.id]);
+    console.log(resultat[0]);
+    return resultat[0];
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
-export default { findFavs }
+export default { findFavs };
